dd79a38e4969c0094993c099bff56c48ee52867a
Ubuntu <spilar@Linux-VM.z2hwj1qxymyehouttamx250gyb.ax.internal.cloudapp.net>
2018-07-29 19:29:18 +0800

    Upload2

--- CMC_Scrape.py
+++ CMC_Scrape.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
-from urllib.request import urlopen
-import time
+import re
 
-subor = "CMC_URLs.txt"
+subor = "20180701.txt"
 
-#Read the text file with CMC snapshot links first
+#Read the text file
 with open(subor) as f:
+	#Skip the first line (header)
+	next(f)
 	for line in f:
-		print (line, end='')
-		html = urlopen(line)
-		print(html.read())
-		
-		#Let's wait a second before fetching another site not look like DOS attack
-		time.sleep(1)
-		
+		#Perform REGEX Search
+		match = re.search(r'(?P<Rank>\d+)\s+(?P<Name>.*)\$(?P<Cap>[0-9,.]+)\s+\$(?P<Price>[0-9.,]+).*\$(?P<Volume>[0-9,]+)',line)
+		#Print the found bits
+		try:
+			print(match.group('Rank'), match.group('Name'), match.group('Cap'), match.group('Price'), match.group('Volume'))
+		except:
+			print("No match")
