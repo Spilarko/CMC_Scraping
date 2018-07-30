a2637505092ff63761eef7921d31d5238f40bda3
Ubuntu <spilar@Linux-VM.z2hwj1qxymyehouttamx250gyb.ax.internal.cloudapp.net>
2018-07-30 23:00:25 +0800

    DB start

--- CMC_Scrape.py
+++ CMC_Scrape.py
@@ -2,17 +2,20 @@
 
 import re
 
-subor = "20180701.txt"
+subor = "20180701"
 
 #Read the text file
 with open(subor) as f:
 	#Skip the first line (header)
 	next(f)
-	for line in f:
+	#Read first 100 lines of the file
+	text = [next(f) for x in range(100)]
+	for line in text:
 		#Perform REGEX Search
 		match = re.search(r'(?P<Rank>\d+)\s+(?P<Name>.*)\$(?P<Cap>[0-9,.]+)\s+\$(?P<Price>[0-9.,]+).*\$(?P<Volume>[0-9,]+)',line)
 		#Print the found bits
 		try:
-			print(match.group('Rank'), match.group('Name'), match.group('Cap'), match.group('Price'), match.group('Volume'))
+			meno = re.search(r'.*\s+(?P<Tick>\w+)',match.group('Name'))
+			print(match.group('Rank'), meno.group('Tick'), match.group('Cap'), match.group('Price'), match.group('Volume'))
 		except:
 			print("No match")
